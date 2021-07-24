b72fc58df6f0cf1edb91850f98e0bc4fbf37f840
r <r@r.local>
2021-07-24 23:29:54 +0800

    add get_all* to init

--- rquote/__init__.py
+++ rquote/__init__.py
@@ -8,5 +8,6 @@ Copyright (c) 2021 Roi ZHAO
 '''
 
 from .rquote import get_price, get_stock_concepts, get_conc_stks
+from .rquote import get_all_concepts, get_all_industries
 from .rquote import get_cn_stocks_by_amount, get_hk_stocks_hotest80, get_us_stocks_hotest30, get_cn_fund_hotest200, get_cn_future
 from .utils import CommonUtils, WebUtils, reqget
