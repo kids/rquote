c1782b05243d11132967b4bbf82be97419a326bb
r <r@r.local>
2021-08-01 06:59:27 +0800

    util add BasicFactors DataFormatter

--- rquote/utils.py
+++ rquote/utils.py
@@ -9,7 +9,7 @@ import random
 import json
 
 logger = logging.getLogger(__name__)
-hdl = logging.FileHandler('/tmp/rproxy.log')
+hdl = logging.FileHandler('/tmp/rquote.log')
 hdl.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 logger.addHandler(hdl)
 logger.addHandler(logging.StreamHandler())
@@ -58,12 +58,14 @@ class WebUtils:
 
     @classmethod
     def headers(cls):
-        headers = {
+        header = {
             'referer': CommonUtils.rand_string(),
-            'user-agent': cls.ua()}
+            'user-agent': cls.ua()
+            }
+        return header
 
     @classmethod
-    def reqget(cls, url, headers, method, proxy=None):
+    def reqget(cls, url, headers, method, proxy=None, proxy_type='http'):
         '''
         request.get() wrapper
         '''
@@ -72,7 +74,7 @@ class WebUtils:
             if proxy is not None:
                 r = requests.get(
                     url, allow_redirects=True, proxies={
-                        'http': proxy})
+                        proxy_type: proxy})
             else:
                 r = requests.get(url, allow_redirects=True)
         except Exception as e:
@@ -85,33 +87,213 @@ class WebUtils:
                 return r.content
 
     @classmethod
-    def test_proxy(cls, proxy: str):
+    def test_proxy(cls, proxy: str, proxy_type='http'):
         '''
         proxy format 'ip:port'
         test baidu.com for cn
-        test google.com for non-cn
+        # test google.com for non-cn (not effective due to DNS hijacking)
         '''
         try:
             r = requests.get(
                 'https://baidu.com',
                 proxies={
-                    'http': proxy},
-                timeout=3)
-        except BaseException:
-            return
+                    proxy_type: proxy},
+                timeout=2)
+        except Exception as e:
+            logger.info(f'test proxy {proxy} negative')
+            return 0
         if r.ok:
-            flag = 'cn'
-            try:
-                r = requests.get(
-                    'https://google.com',
-                    proxies={
-                        'http': proxy},
-                    timeout=3)
-            except BaseException:
-                return
-            if r.ok:
-                flag = 'ncn'
-            return {proxy: flag}
+            logger.info(f'test proxy {proxy} positive')
+            return 1
+
+
+class BasicFactors:
+
+    @staticmethod
+    def break_rise(d) -> float:
+        if d.open[-1] / d.close[-2] > 1.002 and d.close[-1] > d.open[-1]:
+            return round((d.open[-1] - d.close[-2]) / d.close[-2], 2)
+        else:
+            return 0
+
+    @staticmethod
+    def min_resist(d) -> float:
+        sup, pre, pcur = 0, 0, d.close[-1]
+        for i in d.iterrows():
+            p = (i[1].open + i[1].close) / 2
+            if p > pcur:
+                pre += i[1].vol
+            if p < pcur:
+                sup += i[1].vol
+        minres = (sup - pre) / (sup + pre)
+        if abs(minres - 1) < .01 and d.close[-2] < max(d.close[:-2]):
+            minres += .2
+        minres = round(minres, 2)
+        return minres
+
+    @staticmethod
+    def ma_divergence(d) -> float:
+        '''moving averages diverging rate'''
+        d = d.close
+        dlog5 = np.log(d.rolling(5).mean())
+        dlog10 = np.log(d.rolling(10).mean())
+        dlog20 = np.log(d.rolling(20).mean())
+        dlog60 = np.log(d.rolling(60).mean())
+        logmas = pd.DataFrame({'dl5': dlog5, 'dl10': dlog10, 'dl20': dlog20})
+        mstd20 = logmas.std(1).rolling(5).mean()
+        logmas = pd.DataFrame({'dl5': dlog5, 'dl10': dlog10,
+                              'dl20': dlog20, 'dl60': dlog60})
+        # mstd60=logmas.std(1).rolling(5).mean()
+        mv = logmas.max(1) + logmas.min(1) - \
+            logmas.max(1).shift(1) - logmas.min(1).shift(1)
+        return round(mv[-1] / mstd20[-2], 2)
+
+    @staticmethod
+    def vol_extreme(d):
+        d = d.vol
+        v60max = d.rolling(60).max()
+        v60min = d.rolling(60).min()
+        # any in last 3days
+        for i in range(1, 3):
+            if d[-i] > v60max[-i - 1]:
+                return round(d[-i] / v60max[-i - 1], 2)
+            if d[-i] < v60min[-i - 1]:
+                return round(-d[-i] / v60min[-i - 1], 2)
+            else:
+                return 0
+
+    @staticmethod
+    def bias_rate_over_ma60(d):
+        r60 = d.close - d.close.rolling(60).mean()
+        if r60[-1] > 0:
+            return round(r60[-1] / r60.rolling(60).max()[-1], 2)
+        else:
+            return round(-r60[-1] / r60.rolling(60).min()[-1], 2)
+
+    @staticmethod
+    def op_ma(d) -> float:
+        ''' op: ma score'''
+        if len(d) < 22:
+            return
+        d['mv5'] = d.close.rolling(5).mean()
+        d['mv10'] = d.close.rolling(10).mean()
+        d['mv20'] = d.close.rolling(20).mean()
+        d['mv60'] = d.close.rolling(60).mean()
+
+        def ma20(d):
+            ret = 0
+            # .2 for over ma60
+            if d.close[-1] > d.mv60[-1]:
+                ret += 0.2
+            # .2 for all upwards ma's
+            if (d.mv5[-1] > d.mv5[-2] and d.mv10[-1] >
+                    d.mv10[-2] and d.mv20[-1] > d.mv20[-2]):
+                ret += 0.2
+                for j in range(1, 3):
+                    if not (d.close[-j] > d.mv5[-j] and d.close[-j]
+                            > d.mv10[-j] and d.close[-j] > d.mv20[-j]):
+                        return ret
+                for j in range(3, 5):
+                    if (d.close[-j] > d.mv5[-j] and d.close[-j] >
+                            d.mv10[-j] and d.close[-j] > d.mv20[-j]):
+                        return ret
+                # .2 for just rush over ma's (fresh score)
+                ret += 0.2
+            return ret
+        return ma20(d)
+
+    @staticmethod
+    def op_cnt(d, cont_min=3) -> (int):
+        ''' op: count continous bulling days over index'''
+        d.index = pd.DatetimeIndex(d.index)
+        td = (d.p_change_on_sh.rolling(cont_min).min() > 0).astype(int) * \
+            (d.p_change.rolling(cont_min).min() > 0).astype(int)
+        ret = 0 if td[-1] <= 0 else td[-1]
+        # is_first_day = True if td[-2] <= 0 else False
+        return ret
+
+
+class DataFormatter:
+    @staticmethod
+    def s_join_sh_close_change(i, dsh=None, sdate='', edate=''):
+        '''
+        join 2 DataFrames, used in single stock with sh index;
+        keeping columns of 'close' and 'change'
+        '''
+        d = get_price(i, sdate, edate)[2].dropna() if isinstance(i, str) else i
+        d['p_change'] = (d.close - d.close.shift(1)) * 100 / d.close.shift(1)
+        if not len(d) or dsh is None:
+            return d
+        d = d.join(dsh[['open', 'close', 'p_change']], rsuffix='_sh').sort_index()
+        d['p_change_on_sh'] = d['p_change'] - d['p_change_sh']
+        return d
+
+    @staticmethod
+    def sort_keys_by_cossim(df):
+        '''
+        Input:
+            DataFrame with index column as keys
+        sort by dataframe values cosine similarity
+        '''
+        keys=list(df.index)
+        cs=cosine_similarity(df)
+
+        to_sort_keys = [i for i in keys[1:]]
+        sorted_keys = [keys[0]]
+        
+        cid=0
+        for i in range(len(keys)-1):
+            for j in np.argsort(cs[cid])[::-1]:
+                if keys[j] not in sorted_keys:
+                    sorted_keys.append(keys[j])
+                    to_sort_keys.remove(keys[j])
+                    cid=j
+                    break
+        return sorted_keys
+
+    @staticmethod
+    def count_concepts(nhe, nhb, dc):
+        '''
+        merge stock df with concept df with summerized result
+        nhe: stock df with sid, sname
+        nhb: concept df with sid, sname
+        dc: dict of concept {concept: [stock]}
+        TODO abstract it
+        '''
+        nhb.index = nhb.sid
+        nhe.index = nhe.sname
+        nhe['conc'] = ''
+        nhb['list'] = [''] * len(nhb)
+        stks = nhe.sname.tolist()  # candidates
+        stkinconc = []
+        for i, j in nhb.sort_values('imf2', ascending=False)[
+                ['sid', 'sname']].iterrows():
+            ti = []
+            n = get_concept_stks(j.sid, dc=dc)
+            if j.sid not in dc:
+                dc[j.sid] = n
+            for s in n:
+                if s in stks:
+                    stkinconc.append(s)
+                    ti.append(s)
+                    nhe.loc[s, 'conc'] = nhe.loc[s, 'conc'] + ',' + j.sname
+            if ti:
+                nhb.loc[j.sid,
+                        'list'] = '{}/{}:{}'.format(len(ti),
+                                                    len(n),
+                                                    ';'.join(ti))
+        stkr = Counter(stkinconc).most_common(50)
+        stkr = pd.DataFrame(stkr, columns=['sname', 'concs'])
+        nhe.index = nhe.sid
+        nhe = nhe.merge(
+            stkr,
+            on='sname',
+            how='left',
+            suffixes=(
+                '',
+                '_')).fillna('')
+        return nhe, nhb
+
 
 
 class reqget:
