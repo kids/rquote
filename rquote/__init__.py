6a65a4d4072f2d821b47f62659629ff6d6a63945
roizhao <roizhao@tencent.com>
2025-06-11 10:44:17 +0800

    update to date

--- rquote/__init__.py
+++ rquote/__init__.py
@@ -7,7 +7,7 @@ Copyright (c) 2021 Roi ZHAO
 
 '''
 
-from .rquote import get_price, get_stock_concepts, get_concept_stocks
-from .rquote import get_all_concepts, get_all_industries
+from .main import get_price, get_stock_concepts, get_concept_stocks
+from .main import get_all_concepts, get_all_industries
 from .utils import CommonUtils, WebUtils, BasicFactors, DataFormatter, reqget
 from .plots import PlotUtils
