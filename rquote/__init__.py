e1fcf7ae1ed884d28bef0222db8dc5d2af499de1
roizhao <roizhao@tencent.com>
2025-06-19 22:43:17 +0800

    add lost functions to __init__

--- rquote/__init__.py
+++ rquote/__init__.py
@@ -7,7 +7,8 @@ Copyright (c) 2021 Roi ZHAO
 
 '''
 
-from .main import get_price, get_stock_concepts, get_concept_stocks
+from .main import get_price, get_stock_concepts, get_concept_stocks, get_bk_stocks
 from .main import get_all_concepts, get_all_industries
+from .main import get_cn_stock_list, get_hk_stocks_hsi, get_hk_stocks_ggt
 from .utils import WebUtils, BasicFactors
 from .plots import PlotUtils
