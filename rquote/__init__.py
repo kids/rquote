358c956addf91ba37e82737488d9a4d3b7e90772
roizhao <roizhao@tencent.com>
2025-06-20 18:10:32 +0800

    -

--- rquote/__init__.py
+++ rquote/__init__.py
@@ -10,6 +10,6 @@ Copyright (c) 2021 Roi ZHAO
 from .main import get_price, get_stock_concepts, get_concept_stocks, get_bk_stocks
 from .main import get_all_concepts, get_all_industries
 from .main import get_cn_stock_list, get_hk_stocks_hsi, get_hk_stocks_ggt, get_hk_stocks_500
-from .main import get_cn_future_list, get_us_stocks_biggest, get_cn_fund_list
+from .main import get_cn_future_list, get_us_stocks, get_cn_fund_list
 from .utils import WebUtils, BasicFactors
 from .plots import PlotUtils
